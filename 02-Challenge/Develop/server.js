34dfe08debc76bc5dc84a9ec264f63465431c698
Stark <mattstark08@gmail.com>
2023-11-05 21:35:52 -0600

    delete request returning all note objects

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -13,16 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-
+// // returns homepage
 // app.get('/', (req, res) =>
 //   res.sendFile(path.join(__dirname, '/public/index.html'))
 // );
 
+// returns note page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+// returns previous notes
 app.get("/api/notes", (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         const newData = JSON.parse(data)
@@ -35,10 +36,8 @@ app.get("/api/notes", (req, res) => {
 app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
     console.info(`${req.method} request received to add a review`);
-  
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
-  
     // If all the required properties are present
     if (title && text) {
       // Variable for the object we will save
@@ -47,7 +46,6 @@ app.post('/api/notes', (req, res) => {
         text,
         id: uuidv4(),
       };
-  
       // Obtain existing reviews
       fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -55,10 +53,8 @@ app.post('/api/notes', (req, res) => {
         } else {
           // Convert string into JSON object
           const parsedNotes = JSON.parse(data);
-  
           // Add a new review
           parsedNotes.push(newNote);
-  
           // Write updated reviews back to the file
           fs.writeFile(
             './db/db.json',
@@ -70,23 +66,43 @@ app.post('/api/notes', (req, res) => {
           );
         }
       });
-  
       const response = {
         status: 'success',
       };
-  
       console.log(response);
       res.status(201).json(response);
     } else {
       res.status(500).json('Error in posting review');
     }
-  });
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+    
+    console.log(req.body)
+    // returns {}
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        const newData = JSON.parse(data)
+        // res.json(newData)
+       
+        console.log(newData)
+    })
+})
+ 
 
 
+
+
+// returns the homepage
   app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+
+
+
+
+// pseudo server
   app.listen(PORT, () => 
     console.log(`http://localhost:${PORT}`)
   )
